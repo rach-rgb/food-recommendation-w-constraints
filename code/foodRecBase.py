d8446bb580da69c5f1605bf9772bd8393c9254e5
seha park <rachesh@postech.ac.kr>
2021-10-08 17:42:24 +0900

    reformat file

--- code/foodRecBase.py
+++ code/foodRecBase.py
@@ -1,21 +1,21 @@
 from abc import *
 
-class foodRecBase(metaclass=ABCMeta):
+
+class FoodRecBase(metaclass=ABCMeta):
     top_N = 10
-    
+
     # return list of top-N recommended food for uid s.t. includes iid
     @abstractmethod
-    def top_N_const_1(uid, iid):
+    def top_n_const_1(self, uid, iid):
         pass
 
     # return list of top-N recommended food for uid s.t. excludes iid
     @abstractmethod
-    def top_N_const_2(uid, iid):
+    def top_n_const_2(self, uid, iid):
         pass
 
     # return list of top-N recommended food for uid
     # s.t satisfied target nutrient given iid as history
     @abstractmethod
-    def top_N_const_3(uid, iid, target):
+    def top_n_const_3(self, uid, iid, target):
         pass
-        
