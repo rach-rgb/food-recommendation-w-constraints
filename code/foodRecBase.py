ebf74f361affb3379cfca39cb6a833255672d346
seha park <rachesh@postech.ac.kr>
2021-10-11 17:03:10 +0900

    fix: change few methods to abstract method

--- code/foodRecBase.py
+++ code/foodRecBase.py
@@ -1,8 +1,27 @@
 from abc import *
 
 
+# Interface of Food Recommendation System with Constraints
 class FoodRecBase(metaclass=ABCMeta):
     top_N = 10
+    # attributes
+    # algo
+    # train_set
+    # test_set
+    # predictions
+
+    # train with train_set
+    def train(self):
+        self.algo.fit(self.train_set)
+
+    # make prediction with test_set
+    def test(self):
+        self.predictions = self.algo.test(self.test_set)
+
+    # load required data
+    @abstractmethod
+    def get_data(self):
+        pass
 
     # return list of top-N recommended food for uid s.t. includes iid
     @abstractmethod
