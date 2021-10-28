c53dd8451ea6e434ea7aaab8721428bdb2d6f123
rach-rgb <seharhc@gmail.com>
2021-10-29 01:23:05 +0900

    test: TF_algo and fix bugs

--- code/TF_algo.py
+++ code/TF_algo.py
@@ -2,46 +2,45 @@ import numpy as np
 from surprise import PredictionImpossible
 from surprise import SVD
 
-import sys
-sys.path.append('../code')
-import inter_rec
 
-
-class SVD_TF(SVD):
+class SVDtf(SVD):
     def __init__(self):
         SVD.__init__(self)
+        self.i_attr = None
+        self.const = None
+        self.n_err = None
+        self.vio = None  # 0: satisfy ~ 1: not satisfy
 
     # pandas dataframe related to attribute and constraints
-    def set_data(self, attr_data, const_data):
-        self.i_attr = attr_data
-        self.const = const_data
-        self.nutr_error = 5
+    def set_data(self, attr, const, n_err):
+        self.i_attr = attr
+        self.const = const
+        self.n_err = n_err
 
     # check constraints for all (u, i) pairs
     def fit(self, train_set):
         SVD.fit(self, train_set)
 
-        self.sat = np.zeros((train_set.n_users, train_set.n_items))
-        for u in train_set.all_users():
+        self.vio = np.zeros((self.trainset.n_users, self.trainset.n_items))
+        for u in self.trainset.all_users():
             raw_u = int(self.trainset.to_raw_uid(u))
-
             const = self.const.loc[self.const.u == raw_u]
-            if len(const) >= 1:
-                constraint = const.iloc[0]
-            else:
-                constraint = None
-
-            if constraint is None:
+            if len(const) < 1:  # no constraint
                 continue
+            const = const.iloc[0]
 
             for i in train_set.all_items():
                 raw_i = int(self.trainset.to_raw_iid(i))
 
-                self.sat[u][i] = self.check_constraint(raw_i, const)
+                self.vio[u][i] = self.check_constraint(raw_i, const)
+
+        return self
 
     # u, i inner id
-    # raw_u, raw_i raw id
     def estimate(self, u, i):
+        if self.vio[u][i] == 1.0:  # violate constraint
+            return 0.0
+
         known_user = self.trainset.knows_user(u)
         known_item = self.trainset.knows_item(i)
 
@@ -65,37 +64,109 @@ class SVD_TF(SVD):
 
         return est
 
-    # return T/F for constraint 1
-    def check_constraint(self, raw_i, constraint):
-        x = (constraint.i1 is None) or self.include_ingr(raw_i, constraint.i1)
-        if x is False:
-            return 0.0
-        y = (constraint.i2 is None) or self.exclude_ingr(raw_i, constraint.i2)
-        if y is False:
-            return 0.0
-        z = (constraint.hl is None) or self.satisfy_nutr(raw_i, constraint.hl, constraint.nl)
-        if z is False:
+    # modify original SGD slightly
+    def sgd(self, trainset):
+        # user biases
+        cdef np.ndarray[np.double_t] bu
+        # item biases
+        cdef np.ndarray[np.double_t] bi
+        # user factors
+        cdef np.ndarray[np.double_t, ndim=2] pu
+        # item factors
+        cdef np.ndarray[np.double_t, ndim=2] qi
+
+        cdef int u, i, f
+        cdef double r, err, dot, puf, qif
+        cdef double global_mean = self.trainset.global_mean
+
+        cdef double lr_bu = self.lr_bu
+        cdef double lr_bi = self.lr_bi
+        cdef double lr_pu = self.lr_pu
+        cdef double lr_qi = self.lr_qi
+
+        cdef double reg_bu = self.reg_bu
+        cdef double reg_bi = self.reg_bi
+        cdef double reg_pu = self.reg_pu
+        cdef double reg_qi = self.reg_qi
+
+        rng = get_rng(self.random_state)
+
+        bu = np.zeros(trainset.n_users, np.double)
+        bi = np.zeros(trainset.n_items, np.double)
+        pu = rng.normal(self.init_mean, self.init_std_dev,
+                        (trainset.n_users, self.n_factors))
+        qi = rng.normal(self.init_mean, self.init_std_dev,
+                        (trainset.n_items, self.n_factors))
+
+        if not self.biased:
+            global_mean = 0
+
+        for current_epoch in range(self.n_epochs):
+            if self.verbose:
+                print("Processing epoch {}".format(current_epoch))
+            for u, i, r in trainset.all_ratings():
+                # if i violates constraint, ignore error
+                if self.vio[u][i] == 1.0:
+                    err = 0
+                else:
+                    # compute current error
+                    dot = 0  # <q_i, p_u>
+                    for f in range(self.n_factors):
+                        dot += qi[i, f] * pu[u, f]
+                    err = r - (global_mean + bu[u] + bi[i] + dot)
+
+                # update biases
+                if self.biased:
+                    bu[u] += lr_bu * (err - reg_bu * bu[u])
+                    bi[i] += lr_bi * (err - reg_bi * bi[i])
+
+                # update factors
+                for f in range(self.n_factors):
+                    puf = pu[u, f]
+                    qif = qi[i, f]
+                    pu[u, f] += lr_pu * (err * qif - reg_pu * puf)
+                    qi[i, f] += lr_qi * (err * puf - reg_qi * qif)
+
+        self.bu = bu
+        self.bi = bi
+        self.pu = pu
+        self.qi = qi
+
+
+    # check item satisfies constraint
+    # 0: satisfy constraint, 1: not
+    # assume there's only one constraint # TODO
+    def check_constraint(self, item, const):
+        ret = True
+
+        if const['i1'] is not None:
+            ret = self.include_ingr(item, const['i1'])
+        elif const['i2'] is not None:
+            ret = self.exclude_ingr(item, const['i2'])
+
+        if ret:
             return 0.0
         else:
             return 1.0
 
     # return T/F for constraint 1
     def include_ingr(self, fid, iid):
-       return iid in self.i_attr.loc[fid].ingredient_ids
+        return iid in self.i_attr.loc[fid].ingredient_ids
 
     # return T/F for constraint 2
     def exclude_ingr(self, fid, iid):
-        return not iid in self.i_attr.loc[fid].ingredient_ids
+        return iid not in self.i_attr.loc[fid].ingredient_ids
 
     # return T/F for constraint 3
+    # TODO
     def satisfy_nutr(self, fid, hid, target_nutr):
         nutr = self.i_attr.loc[fid].nutrition
         nutr_hist = self.i_attr.loc[hid].nutrition
         for i in range(0, len(nutr)):
             target = target_nutr[i] - float(nutr_hist[i])
             value = float(nutr[i])
-            if value < (target * (1 - (self.nutr_error / 100))):
+            if value < (target * (1 - (self.n_err / 100))):
                 return False
-            if (target * (1 + (self.nutr_error / 100))) < value:
+            if (target * (1 + (self.n_err / 100))) < value:
                 return False
         return True
---
+++ code/test/test_TF_algo.py
@@ -0,0 +1,87 @@
+import unittest
+from sys import path
+import pandas as pd
+import numpy as np
+
+# setting path
+path.append('../code')
+
+import inter_rec
+import TF_algo
+
+
+class TestSVDTF(unittest.TestCase):
+    def setUp(self):
+        self.algo = TF_algo.SVDtf()
+        self.rec = inter_rec.InterRec('./data/rate.csv', './data/attr.csv', './data/const.csv', self.algo)
+        self.rec.get_data()
+
+    # test set_data
+    def test_set_data(self):
+        # check algo() object attr
+        const = self.algo.const.loc[self.rec.const.u == 0].iloc[0]
+        self.assertEqual(1, const['i1'])
+        self.assertEqual(None, const['i2'])
+        self.assertEqual(const['nl'], "")
+
+        # check algo() object const
+        attr = self.algo.i_attr.loc[0]
+        self.assertIn(1, attr.ingredient_ids)
+        self.assertNotIn(4, attr.ingredient_ids)
+        self.assertEqual(attr.nutrition[0], 200)
+
+        # check algo() n_err
+        self.assertEqual(self.rec.n_err, self.algo.n_err)
+
+    # test include_ingr
+    def test_include_ingr(self):
+        self.assertTrue(self.algo.include_ingr(0, 1))
+        self.assertFalse(self.algo.include_ingr(0, 4))
+        self.assertFalse(self.algo.include_ingr(0, '1'))
+
+    # test exclude_ingr
+    def test_exclude_ingr(self):
+        self.assertTrue(self.algo.exclude_ingr(0, 4))
+        self.assertFalse(self.algo.exclude_ingr(0, 1))
+        self.assertTrue(self.algo.exclude_ingr(0, '1'))
+
+    # test satisfy_nutr
+    # TODO
+
+    # test check_constraint
+    # TODO add constraint 3
+    def test_check_constraint(self):
+        const1 = self.algo.const.loc[self.rec.const.u == 0].iloc[0]
+        const2 = self.algo.const.loc[self.rec.const.u == 1].iloc[0]
+
+        self.assertEqual(0.0, self.algo.check_constraint(0, const1))
+        self.assertEqual(1.0, self.algo.check_constraint(2, const1))
+        self.assertEqual(0.0, self.algo.check_constraint(0, const2))
+        self.assertEqual(1.0, self.algo.check_constraint(2, const2))
+
+    # test fit()
+    def test_fit(self):
+        self.rec.train()  # this calls self.algo.fit()
+        sat = self.algo.vio
+        inner_uid = self.algo.trainset.to_inner_uid
+        inner_iid = self.algo.trainset.to_inner_iid
+
+        self.assertEqual(0.0, sat[inner_uid('0')][inner_iid('0')])
+        self.assertEqual(0.0, sat[inner_uid('0')][inner_iid('0')])
+        self.assertEqual(0.0, sat[inner_uid('0')][inner_iid('1')])
+        self.assertEqual(1.0, sat[inner_uid('0')][inner_iid('2')])
+        self.assertEqual(0.0, sat[inner_uid('1')][inner_iid('0')])
+        self.assertEqual(1.0, sat[inner_uid('1')][inner_iid('2')])
+
+    # test estimate()
+    def test_estimate(self):
+        self.rec.train()
+        inner_uid = self.algo.trainset.to_inner_uid
+        inner_iid = self.algo.trainset.to_inner_iid
+
+        self.assertNotEqual(0.0, self.algo.estimate(inner_uid('0'), inner_iid('0')))
+        self.assertEqual(0.0, self.algo.estimate(inner_uid('0'), inner_iid('2')))
+
+
+if __name__ == '__main__':
+    unittest.main()
